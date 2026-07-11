165aac2edcfbdf7cfe13079015ccc8bcf74a8780
KingBes <48203677+KingBes@users.noreply.github.com>
2026-07-11 14:43:19 +0800

    Update macos.v

--- compile/macos.v
+++ compile/macos.v
@@ -31,11 +31,10 @@ pub fn macos_build(conf common.BnyConfig) ! {
 		}
 		r2 := os.execute('productsign --sign - ${pkg_tmp} ${pkg_path}')
 		if r2.exit_code != 0 {
-			println(term.yellow('productsign失败,使用未签名pkg'))
+			println(term.yellow('未签名,用户可自行签名'))
 			os.cp(pkg_tmp, pkg_path)!
-		} else {
-			os.rm(pkg_tmp)!
 		}
+		os.rm(pkg_tmp)!
 	}
 	println(term.green('编译完成:' + common.shell_path(conf.name + '.(pkg/dmg)')))
 }
